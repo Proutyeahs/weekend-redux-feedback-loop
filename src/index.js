02f9e3ba9bcd5f6cfd3b546673c2210e14471912
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-08-19 15:56:23 -0500

    made reducer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const surveyResults = (state = [], action) => {
+    if (action.type === 'FEELING') {
+        return [...state, action.payload]
+    }
+    if (action.type === 'UNDERSTAND') {
+        return [...state, action.payload]
+    }
+    if (action.type === 'SUPPORT') {
+        return [...state, action.payload]
+    }
+    if (action.type === 'COMMENT') {
+        return [...state, action.payload]
+    }
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        surveyResults
+    }),
+    applyMiddleware(logger),
+  );
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
